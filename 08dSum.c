511f9bb7fd4cf3d2b14bfba0846931e85b3def75
Pallavip0903 <158282266+Pallavip0903@users.noreply.github.com>
2024-02-05 23:15:56 +0530

    Sum

---
+++ 08dSum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+ int main()
+ {
+ int No=0; temp=0; dSum=0;
+ 
+ printf("\n Enter a number:");
+ scanf("%d", &No) ;
+  for (Temp = No; Temp > 0; dSum += (Temp=%10) , Temp/=10)
+  printf("/n/n Digit sum = %d %d.",No, dSum); 
+ getch();
+ return 0;
+ }
