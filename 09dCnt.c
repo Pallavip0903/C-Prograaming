926430ccbf4f436a59e1ebc003414e4ad451fa27
Pallavip0903 <158282266+Pallavip0903@users.noreply.github.com>
2024-02-07 22:00:43 +0530

    Count. C

---
+++ 09dCnt.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+ int main()
+ {
+ int Num=0; temp=0, dcount=0;
+ printf("\n Enter a number:");
+ scanf("%d", &Num) ;
+ temp=Num;
+ while(Temp >0) 
+ 
+ {
+ dcount++;
+ }
+ temp=temp/10;
+ }
+ 
+ printf("/n/n Digits in%d = %d",Num,dcount);
+ getch() ;
+ return 0;
+ }
